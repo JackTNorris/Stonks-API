0338a2699cd60d9d0d62c643b40a37086a5d5a4a
Jack Norris <jacknorris@jacks-mbp.ddns.uark.edu>
2020-08-27 18:36:34 -0500

    Testing express with heroku try 3

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-import express from './src/loaders';
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.send("hello"))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
