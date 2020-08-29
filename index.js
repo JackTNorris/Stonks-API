3b73ef3ed3f7f7227ee2530ee7f573d47973f4c1
Jack Norris <jacknorris@jacks-mbp.ddns.uark.edu>
2020-08-29 00:55:00 -0500

    Testing ebitda with heroku part 7

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ getEbitda = async (ticker, res) => {
           const dom = new JSDOM(html);
           let tempArr = dom.window.document.getElementsByClassName('Fw(500) Ta(end) Pstart(10px) Miw(60px)');
           const ebitda = tempArr[38].textContent;
-          res.send(html);
+          res.send("SUCCESS");
           //console.log(tempArr[38].textContent);
-      }).catch(err => res.send(err))
-  }).catch(err => res.send(err));
+      }).catch(err => res.send("ERROR!"))
+  }).catch(err => res.send("ERROR!"));
 }
 
 
