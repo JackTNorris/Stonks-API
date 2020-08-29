fccd1a5a9bdfe1edf97fd1ee4a96efed4fdcaaa7
Jack Norris <jacknorris@jacks-mbp.ddns.uark.edu>
2020-08-29 00:45:27 -0500

    Testing ebitda with heroku part 4

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-import {getEbitda} from './src/api/ebitda';
+
+getEbitda = async (ticker, res) => {
+  puppeteer.launch({headless: true}).then(async browser => {
+      let page = await browser.newPage();
+      page.setViewport({ width: 1366, height: 768 });
+      const timeout = 30000;
+      const url = `https://finance.yahoo.com/quote/${ticker}/key-statistics?ltr=1`;
+      await page.goto(url, {waitUntil: 'load'});
+  
+      page.content().then((html) => {
+          const dom = new JSDOM(html);
+          let tempArr = dom.window.document.getElementsByClassName('Fw(500) Ta(end) Pstart(10px) Miw(60px)');
+          const ebitda = tempArr[38].textContent;
+          res.send(ebitda);
+          //console.log(tempArr[38].textContent);
+      }).catch(err => res.send(err))
+  }).catch(err => res.send(err));
+}
+
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
